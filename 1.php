3a67a33262eedef8f4266395ce6ee273b0321ce1
xiaojinwen <1058330410@qq.com>
2019-01-02 21:20:03 +0800

    更新

---
+++ 1.php
@@ -0,0 +1,8 @@
+<?php
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Credentials:true'); 
+header('Content-Type:application:json;charset=utf8');  
+header('Access-Control-Allow-Methods:POST,GET'); 
+header('Access-Control-Allow-Headers:Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With'); 
+echo "1058330410";
+?>
