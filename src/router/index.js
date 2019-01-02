f6afc32f2f2395634858072ec8b474dc6000ca68
xiaojinwen <1058330410@qq.com>
2019-01-02 21:47:40 +0800

    更新

--- src/router/index.js
+++ src/router/index.js
@@ -1,107 +1,67 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import Recommend from 'components/recommend/recommend'
-// import Singer from 'components/singer/singer'
-// import Rank from 'components/rank/rank'
-// import Search from 'components/search/search'
-// import SingerDetail from 'components/singer-detail/singer-detail'
-// import Disc from 'components/disc/disc'
-// import TopList from 'components/top-list/top-list'
-// import UserCenter from 'components/user-center/user-center'
-Vue.use(VueRouter)
 
-const Recommend = (resolve) => {
-  import('components/recommend/recommend').then((module) => {
-    resolve(module)
-  })
-}
-const Singer = (resolve) => {
-  import('components/singer/singer').then((module) => {
-    resolve(module)
-  })
-}
-const Rank = (resolve) => {
-  import('components/rank/rank').then((module) => {
-    resolve(module)
-  })
-}
-const Search = (resolve) => {
-  import('components/search/search').then((module) => {
-    resolve(module)
-  })
-}
-const SingerDetail = (resolve) => {
-  import('components/singer-detail/singer-detail').then((module) => {
-    resolve(module)
-  })
-}
-const Disc = (resolve) => {
-  import('components/disc/disc').then((module) => {
-    resolve(module)
-  })
-}
-const TopList = (resolve) => {
-  import('components/top-list/top-list').then((module) => {
-    resolve(module)
-  })
-}
-const UserCenter = (resolve) => {
-  import('components/user-center/user-center').then((module) => {
-    resolve(module)
-  })
-}
+Vue.use(VueRouter)
+const Recommend = () => import('components/recommend/recommend')
+const Singer = () => import('components/singer/singer')
+const Rank = () => import('components/rank/rank')
+const Search = () => import('components/search/search')
+const SingerDetail = () => import('components/singer-detail/singer-detail')
+const Disc = () => import('components/disc/disc')
+const TopList = () => import('components/top-list/top-list')
+const UserCenter = () => import('components/user-center/user-center')
 
 export default new VueRouter({
-  routes: [
-    {
-      path: '/',
-      redirect: '/recommend'
-    },
-    {
-      path: '/recommend',
-      name: 1,
-      component: Recommend,
-      children: [{
-        path: ':id',
-        component: Disc
-      }]
-    },
-    {
-      path: '/singer',
-      name: 2,
-      component: Singer,
-      children: [
+    routes: [
         {
-          path: ':id',
-          component: SingerDetail
-        }
-      ]
-    },
-    {
-      path: '/rank',
-      name: 3,
-      component: Rank,
-      children: [
+            path: '/',
+            redirect: '/recommend'
+        },
         {
-          path: ':id',
-          component: TopList
-        }
-      ]
-    },
-    {
-      path: '/search',
-      name: 4,
-      component: Search,
-      children: [
+            path: '/recommend',
+            name: 1,
+            component: Recommend,
+            children: [{
+                path: ':id',
+                component: Disc
+            }]
+        },
         {
-          path: ':id',
-          component: SingerDetail
+            path: '/singer',
+            name: 2,
+            component: Singer,
+            children: [
+                {
+                    path: ':id',
+                    component: SingerDetail
+                }
+            ]
+        },
+        {
+            path: '/rank',
+            name: 3,
+            component: Rank,
+            children: [
+                {
+                    path: ':id',
+                    component: TopList
+                }
+            ]
+        },
+        {
+            path: '/search',
+            name: 4,
+            component: Search,
+            children: [
+                {
+                    path: ':id',
+                    component: SingerDetail
+                }
+            ]
+        },
+        {
+            path: '/user',
+            component: UserCenter
         }
-      ]
-    },
-    {
-      path: '/user',
-      component: UserCenter
-    }
-  ]
+    ]
 })
